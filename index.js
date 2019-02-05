26e0a9d3ae029735a44aba8a3931b1b47ec47c9f
Samuel Cornet <samuel@noukod.com>
2019-02-05 20:58:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 // function logDriverNames(drivers) {
 //   drivers.forEach(driver => console.log(driver.name))
 // }
-// 
+//
 // function logDriversByHometown(drivers, hometown) {
 //   drivers.forEach(function(driver) {if (driver.hometown === hometown) {console.log(driver.name) } })
 // }
-// 
+//
 // function driversByRevenue(drivers) {
 //     return drivers.slice().sort( (a, b) => {return a.revenue - b.revenue} )
 // }
@@ -19,8 +19,8 @@
 // function totalRevenue(drivers) {
 //   return drivers.reduce((total, driver) => {return total + driver.revenue}, 0)
 // }
-// 
-// 
+//
+//
 // function averageRevenue(drivers) {
 //   return totalRevenue(drivers) / drivers.length
 // }
